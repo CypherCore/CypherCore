f4a74966135daa987b84b685d67ec2b90d1a3c61
Elle <luigibalotta@icloud.com>
2017-11-28 17:17:39 +0100

    typ

--- Source/Game/DataStorage/Structs/B_Records.cs
+++ Source/Game/DataStorage/Structs/B_Records.cs
@@ -110,10 +110,9 @@ namespace Game.DataStorage
         public LocalizedString FemaleText;
         public ushort[] EmoteID = new ushort[3];
         public ushort[] EmoteDelay = new ushort[3];
-        public ushort UnkEmoteID;
+		public uint SoundID;
+		public uint UnkMoP1;
+		public uint UnkMoP2;
         public byte Language;
-        public byte Type;
-        public uint[] SoundID = new uint[2];
-        public uint PlayerConditionID;
     }
 }
