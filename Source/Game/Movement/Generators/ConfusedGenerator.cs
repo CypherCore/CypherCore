28c0ef9d03dbb888a77781d44b1ad0d524c2e368
hondacrx <prelude_20032002@yahoo.com>
2018-01-02 13:02:21 -0500

    Happy New Year!!!!

--- Source/Game/Movement/Generators/ConfusedGenerator.cs
+++ Source/Game/Movement/Generators/ConfusedGenerator.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2012-2017 CypherCore <http://github.com/CypherCore>
+ * Copyright (C) 2012-2018 CypherCore <http://github.com/CypherCore>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -68,6 +68,7 @@ namespace Game.Movement
             owner.StopMoving();
             owner.AddUnitState(UnitState.Confused | UnitState.ConfusedMove);
         }
+
         public override bool DoUpdate(T owner, uint time_diff)
         {
             if (owner.HasUnitState(UnitState.Root | UnitState.Stunned | UnitState.Distracted))
