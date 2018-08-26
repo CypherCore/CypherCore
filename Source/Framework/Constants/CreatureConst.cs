8fdf1deb4fa6068f332ca98fcabf374e4f1295f8
hondacrx <prelude_20032002@yahoo.com>
2018-08-26 01:12:48 -0400

    Core/Units: Defined new npc flag

--- Source/Framework/Constants/CreatureConst.cs
+++ Source/Framework/Constants/CreatureConst.cs
@@ -113,45 +113,46 @@ namespace Framework.Constants
     public enum NPCFlags : ulong
     {
         None = 0x00,
-        Gossip = 0x01,
-        QuestGiver = 0x02,
+        Gossip = 0x01,     // 100%
+        QuestGiver = 0x02,     // 100%
         Unk1 = 0x04,
         Unk2 = 0x08,
-        Trainer = 0x10,
-        TrainerClass = 0x20,
-        TrainerProfession = 0x40,
-        Vendor = 0x80,
-        VendorGeneral = 0x100,
-        VendorFood = 0x200,
-        VendorPoison = 0x400,
-        VendorReagent = 0x800,
-        Repair = 0x1000,
-        FlightMaster = 0x2000,
-        SpiritHealer = 0x4000,
-        SpiritGuide = 0x8000,
-        Innkeeper = 0x10000,
-        Banker = 0x20000,
-        Petitioner = 0x40000,
-        TabardDesigner = 0x80000,
-        BattleMaster = 0x100000,
-        Auctioneer = 0x200000,
-        StableMaster = 0x400000,
-        GuildBanker = 0x800000,
-        SpellClick = 0x1000000,
-        PlayerVehicle = 0x2000000,
-        Mailbox = 0x4000000,
-        ArtifactPowerRespec = 0x8000000,
-        Transmogrifier = 0x10000000,
-        VaultKeeper = 0x20000000,
-        WildBattlePet = 0x0040000000,
-        BlackMarket = 0x80000000,
+        Trainer = 0x10,     // 100%
+        TrainerClass = 0x20,     // 100%
+        TrainerProfession = 0x40,     // 100%
+        Vendor = 0x80,     // 100%
+        VendorAmmo = 0x100,     // 100%, General Goods Vendor
+        VendorFood = 0x200,     // 100%
+        VendorPoison = 0x400,     // Guessed
+        VendorReagent = 0x800,     // 100%
+        Repair = 0x1000,     // 100%
+        FlightMaster = 0x2000,     // 100%
+        SpiritHealer = 0x4000,     // Guessed
+        SpiritGuide = 0x8000,     // Guessed
+        Innkeeper = 0x10000,     // 100%
+        Banker = 0x20000,     // 100%
+        Petitioner = 0x40000,     // 100% 0xc0000 = Guild Petitions, 0x40000 = Arena Team Petitions
+        TabardDesigner = 0x80000,     // 100%
+        BattleMaster = 0x100000,     // 100%
+        Auctioneer = 0x200000,     // 100%
+        StableMaster = 0x400000,     // 100%
+        GuildBanker = 0x800000,     //
+        SpellClick = 0x1000000,     //
+        PlayerVehicle = 0x2000000,     // Players With Mounts That Have Vehicle Data Should Have It Set
+        Mailbox = 0x4000000,     // Mailbox
+        ArtifactPowerRespec = 0x8000000,     // Artifact Powers Reset
+        Transmogrifier = 0x10000000,     // Transmogrification
+        VaultKeeper = 0x20000000,     // Void Storage
+        WildBattlePet = 0x40000000,     // Pet That Player Can Fight (Battle Pet)
+        BlackMarket = 0x80000000,     // Black Market
         ItemUpgradeMaster = 0x100000000,
         GarrisonArchitect = 0x200000000,
         Steering = 0x400000000,
         ShipmentCrafter = 0x1000000000,
         GarrisonMissionNpc = 0x2000000000,
         TradeskillNpc = 0x4000000000,
-        BlackMarketView = 0x8000000000
+        BlackMarketView = 0x8000000000,
+        ContributionCollector = 0x40000000000,
     }
 
     public enum CreatureTypeFlags : uint
