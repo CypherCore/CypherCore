86e1065da09fe6a3345675b8b9159fbe8de2f2ca
hondacrx <prelude_20032002@yahoo.com>
2019-12-04 23:19:32 -0500

    Core: Updated allowed build to 8.2.5.32722
    Port From (https://github.com/TrinityCore/TrinityCore/commit/65b91375e0a3b01eaacab77f973429b544c63a61)

--- Source/Framework/Realm/RealmManager.cs
+++ Source/Framework/Realm/RealmManager.cs
@@ -122,6 +122,7 @@ public class RealmManager : Singleton<RealmManager>
     // List of client builds for verbose version info in realmlist packet
     RealmBuildInfo[] ClientBuilds =
     {
+        new RealmBuildInfo(32722, 8, 2, 5, ' ' ),
         new RealmBuildInfo(32638, 8, 2, 5, ' ' ),
         new RealmBuildInfo(32580, 8, 2, 5, ' ' ),
         new RealmBuildInfo(32494, 8, 2, 5, ' ' ),
