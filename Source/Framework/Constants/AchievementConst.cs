d518c9c1d0279468b43327415f039b6fcd4af4d0
hondacrx <prelude_20032002@yahoo.com>
2021-06-04 14:08:55 -0400

    Core/Criteria: Add all values of CriteriaFlags
    Port From (https://github.com/TrinityCore/TrinityCore/commit/2629015011ee1d6e3e50a035dc57e08231ff726a)

--- Source/Framework/Constants/AchievementConst.cs
+++ Source/Framework/Constants/AchievementConst.cs
@@ -443,12 +443,12 @@ namespace Framework.Constants
 
     public enum CriteriaFlags
     {
-        ShowProgressBar = 0x01,
-        Hidden = 0x02,
-        FailAchievement = 0x04,
-        ResetOnStart = 0x08,
-        IsDate = 0x10,
-        MoneyCounter = 0x20
+        FailAchievement = 0x01, // Fail Achievement
+        ResetOnStart = 0x02, // Reset on Start
+        ServerOnly = 0x04, // Server Only
+        AlwaysSaveToDB = 0x08, // Always Save to DB (Use with Caution)
+        AllowCriteriaDecrement = 0x10, // Allow criteria to be decremented
+        IsForQuest = 0x20  // Is For Quest
     }
 
     public enum CriteriaTypes : byte
