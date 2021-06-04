cd9aab7d526f2b8b45e226efd2179d066cf28e52
hondacrx <prelude_20032002@yahoo.com>
2021-06-04 14:09:55 -0400

    Core/Criteria: Add all values of CriteriaTreeFlags
    Port From (https://github.com/TrinityCore/TrinityCore/commit/5543908e52bec8d62aca33b4f04171dc19dfe275)

--- Source/Framework/Constants/AchievementConst.cs
+++ Source/Framework/Constants/AchievementConst.cs
@@ -28,12 +28,19 @@ namespace Framework.Constants
 
     public enum CriteriaTreeFlags
     {
-        ProgressBar = 0x0001,
-        ProgressIsDate = 0x0004,
-        ShowCurrencyIcon = 0x0008,
-        AllianceOnly = 0x0200,
-        HordeOnly = 0x0400,
-        ShowRequiredCount = 0x0800
+        ProgressBar = 0x0001, // Progress Bar
+        DoNotDisplay = 0x0002, // Do Not Display
+        IsDate = 0x0004, // Is a Date
+        IsMoney = 0x0008, // Is Money
+        ToastOnComplete = 0x0010, // Toast on Complete
+        UseObjectsDescription = 0x0020, // Use Object's Description
+        ShowFactionSpecificChild = 0x0040, // Show faction specific child
+        DisplayAllChildren = 0x0080, // Display all children
+        AwardBonusRep = 0x0100, // Award Bonus Rep (Hack!!)
+        AllianceOnly = 0x0200, // Treat this criteria or block as Alliance
+        HordeOnly = 0x0400, // Treat this criteria or block as Horde
+        DisplayAsFraction = 0x0800, // Display as Fraction
+        IsForQuest = 0x1000  // Is For Quest
     }
 
     public enum CriteriaTreeOperator
