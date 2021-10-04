6bc25f322745e1bf1a97688f3e0731909e84b6a6
hondacrx <prelude_20032002@yahoo.com>
2021-10-03 22:24:02 -0400

    Scripts/Spells: Implemented Sigil of Silence/Misery/Flame/Chains
    Port From (https://github.com/TrinityCore/TrinityCore/commit/3080c37952377b517b1b753e8ad2ebf4836fd088)

--- Source/Scripts/Spells/DemonHunter.cs
+++ Source/Scripts/Spells/DemonHunter.cs
@@ -19,12 +19,20 @@ using Framework.Constants;
 using Game.Entities;
 using Game.Scripting;
 using Game.Spells;
+using Game.AI;
 
 namespace Scripts.Spells.DemonHunter
 {
     struct SpellIds
     {
         public const uint ChaosStrikeEnergize = 193840;
+        public const uint SigilOfChainsGrip = 208674;
+        public const uint SigilOfChainsSlow = 204843;
+        public const uint SigilOfChainsTargetSelect = 204834;
+        public const uint SigilOfChainsVisual = 208673;
+        public const uint SigilOfFlameAoe = 204598;
+        public const uint SigilOfMiseryAoe = 207685;
+        public const uint SigilOfSilenceAoe = 204490;
     }
 
     [Script] // 197125 - Chaos Strike
@@ -47,6 +55,84 @@ namespace Scripts.Spells.DemonHunter
         public override void Register()
         {
             OnEffectProc.Add(new EffectProcHandler(HandleEffectProc, 0, AuraType.ProcTriggerSpell));
+        }
+    }
+
+    // 204596 - Sigil of Flame
+    // 207684 - Sigil of Misery
+    // 202137 - Sigil of Silence
+    [Script("areatrigger_dh_sigil_of_silence", SpellIds.SigilOfSilenceAoe)]
+    [Script("areatrigger_dh_sigil_of_misery", SpellIds.SigilOfMiseryAoe)]
+    [Script("areatrigger_dh_sigil_of_flame", SpellIds.SigilOfFlameAoe)]
+    class areatrigger_dh_generic_sigil : AreaTriggerEntityScript
+    {
+        uint _trigger;
+
+        public areatrigger_dh_generic_sigil(string script, uint trigger) : base(script)
+        {
+            _trigger = trigger;
+        }
+
+        class areatrigger_dh_generic_sigilAI : AreaTriggerAI
+        {
+            uint _trigger;
+
+            public areatrigger_dh_generic_sigilAI(AreaTrigger at, uint trigger) : base(at)
+            {
+                _trigger = trigger;
+            }
+
+            public override void OnRemove()
+            {
+                Unit caster = at.GetCaster();
+                if (caster != null)
+                    caster.CastSpell(at.GetPosition(), _trigger, new CastSpellExtraArgs());
+            }
+        }
+
+        public override AreaTriggerAI GetAI(AreaTrigger at)
+        {
+            return new areatrigger_dh_generic_sigilAI(at, _trigger);
+        }
+    }
+
+    [Script] // 208673 - Sigil of Chains
+    class spell_dh_sigil_of_chains : SpellScript
+    {
+        public override bool Validate(SpellInfo spellInfo)
+        {
+            return ValidateSpellInfo(SpellIds.SigilOfChainsSlow, SpellIds.SigilOfChainsGrip);
+        }
+
+        void HandleEffectHitTarget(uint effIndex)
+        {
+            WorldLocation loc = GetExplTargetDest();
+            if (loc != null)
+            {
+                GetCaster().CastSpell(GetHitUnit(), SpellIds.SigilOfChainsSlow, new CastSpellExtraArgs(true));
+                GetHitUnit().CastSpell(loc.GetPosition(), SpellIds.SigilOfChainsGrip, new CastSpellExtraArgs(true));
+            }
+        }
+
+        public override void Register()
+        {
+            OnEffectHitTarget.Add(new EffectHandler(HandleEffectHitTarget, 0, SpellEffectName.Dummy));
+        }
+    }
+
+    [Script] // 202138 - Sigil of Chains
+    class areatrigger_dh_sigil_of_chains : AreaTriggerAI
+    {
+        public areatrigger_dh_sigil_of_chains(AreaTrigger at) : base(at) { }
+
+        public override void OnRemove()
+        {
+            Unit caster = at.GetCaster();
+            if (caster != null)
+            {
+                caster.CastSpell(at.GetPosition(), SpellIds.SigilOfChainsVisual, new CastSpellExtraArgs());
+                caster.CastSpell(at.GetPosition(), SpellIds.SigilOfChainsTargetSelect, new CastSpellExtraArgs());
+            }
         }
     }
 }
