8f85ff92503037d644597ce6b1d1fe0fa87e8f50
hondacrx <prelude_20032002@yahoo.com>
2021-11-18 19:41:57 -0500

    Core/Spells: Malleable Goo need be negative
    Port From (https://github.com/TrinityCore/TrinityCore/commit/748e8463c7abc218075f7d70a234b0aaf3d07900)

--- Source/Game/Spells/SpellInfo.cs
+++ Source/Game/Spells/SpellInfo.cs
@@ -3296,6 +3296,7 @@ namespace Game.Spells
                         case 64412: // Phase Punch, Algalon the Observer, Ulduar
                         case 72410: // Rune of Blood, Saurfang, Icecrown Citadel
                         case 71204: // Touch of Insignificance, Lady Deathwhisper, Icecrown Citadel
+                        case 70853: // Malleable Goo, Professor Putricide, Icecrown Citadel
                             return false;
                         case 24732: // Bat Costume
                         case 30877: // Tag Murloc
