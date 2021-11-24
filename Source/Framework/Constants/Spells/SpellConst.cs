1adfbb7a8866e8e01bccb23bcff49586c4bd7a5d
hondacrx <prelude_20032002@yahoo.com>
2021-11-23 20:59:31 -0500

    Fix build

--- Source/Framework/Constants/Spells/SpellConst.cs
+++ Source/Framework/Constants/Spells/SpellConst.cs
@@ -2812,7 +2812,9 @@ namespace Framework.Constants
     {
         None = 0x0,
         IncludeGCD = 0x1,  // Starts GCD in addition to normal cooldown specified in the packet
-        IncludeEventCooldowns = 0x2   // Starts GCD for spells that should start their cooldown on events, requires SPELL_COOLDOWN_FLAG_INCLUDE_GCD set
+        IncludeEventCooldowns = 0x2,   // Starts GCD for spells that should start their cooldown on events, requires SPELL_COOLDOWN_FLAG_INCLUDE_GCD set
+        LossOfControlUi = 0x4,  // Shows interrupt cooldown in loss of control ui
+        OnHold = 0x8   // Forces cooldown to behave as if SpellInfo::IsCooldownStartedOnEvent was true
     }
 
     public enum SpellAreaFlag
