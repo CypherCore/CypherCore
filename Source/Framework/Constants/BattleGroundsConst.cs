5d315fd84d8ddd59ec0477f1e6bedbcbbd626613
hondacrx <prelude_20032002@yahoo.com>
2022-05-31 11:48:24 -0400

    Core/Battlegrounds: Updated battleground join results enum
    Port From (https://github.com/TrinityCore/TrinityCore/commit/15fe4e09d4dda16f23100412670dbdfbae5c5e61)

--- Source/Framework/Constants/BattleGroundsConst.cs
+++ Source/Framework/Constants/BattleGroundsConst.cs
@@ -313,7 +313,7 @@ namespace Framework.Constants
         BattledgroundQueuedForRated = 6,        // You Cannot Queue For Another Battle While Queued For A Rated Arena Match
         TeamLeftQueue = 7,        // Your Team Has Left The Arena Queue
         NotInBattleground= 8,        // You Can'T Do That In A Battleground.
-        JoinXpGain = 9,        // Wtf, Doesn'T Exist In Client...
+        JoinXpGain = 9,        // Cannot join as a group unless all the members of your party have the same XP gain setting.
         JoinRangeIndex = 10,       // Cannot Join The Queue Unless All Members Of Your Party Are In The Same BattlegroundLevel Range.
         JoinTimedOut = 11,       // %S Was Unavailable To Join The Queue. (Uint64 Guid Exist In Client Cache)
                                  //JoinTimedOut               = 12,       // Same As 11
@@ -321,21 +321,32 @@ namespace Framework.Constants
         LfgCantUseBattleground= 14,       // You Cannot Queue For A BattlegroundOr Arena While Using The Dungeon System.
         InRandomBg = 15,       // Can'T Do That While In A Random BattlegroundQueue.
         InNonRandomBg = 16,       // Can'T Queue For Random BattlegroundWhile In Another BattlegroundQueue.
-        BgDeveloperOnly = 17,
-        InvitationDeclined = 18,
-        MeetingStoneNotFound = 19,
-        WargameRequestFailure = 20,
-        BattlefieldTeamPartySize = 22,
-        NotOnTournamentRealm = 23,
-        PlayersFromDifferentRealms = 24,
-        RemoveFromPvpQueueGrantLevel = 33,
-        RemoveFromPvpQueueFactionChange = 34,
-        JoinFailed = 35,
-        DupeQueue = 43,
-        JoinNoValidSpecForRole = 44,
-        JoinRespec = 45,
-        AlreadyUsingLFGList = 46,
-        JoinMustCompleteQuest = 47
+        BgDeveloperOnly = 17,       // This Battleground Is Only Available For Developer Testing At This Time.
+        BattlegroundInvitationDeclined = 18,       // Your War Game Invitation Has Been Declined
+        MeetingStoneNotFound = 19,       // Player Not Found.
+        WargameRequestFailure = 20,       // War Game Request Failed
+        BattlefieldTeamPartySize = 22,       // Incorrect Party Size For This Battlefield.
+        NotOnTournamentRealm = 23,       // Not Available On A Tournament Realm.
+        BattlegroundPlayersFromDifferentRealms = 24,       // You Cannot Queue For A Battleground While Players From Different Realms Are In Your Party.
+        BattlegroundJoinLevelup = 33,       // You Have Been Removed From A Pvp Queue Because You Have Gained A Level.
+        RemoveFromPvpQueueFactionChange = 34,       // You Have Been Removed From A Pvp Queue Because You Changed Your Faction.
+        BattlegroundJoinFailed = 35,       // Join As A Group Failed
+        BattlegroundDupeQueue = 43,       // Someone In Your Group Is Already Queued For That.
+        BattlegroundJoinNoValidSpecForRole = 44,       // Role Check Failed Because One Of Your Party Members Selected An Invalid Role.
+        BattlegroundJoinRespec = 45,       // You Have Been Removed From A Pvp Queue Because Your Specialization Changed.
+        AlreadyUsingLfgList = 46,       // You Can'T Do That While Using Premade Groups.
+        BattlegroundJoinMustCompleteQuest = 47,       // You Have Been Removed From A Pvp Queue Because Someone Is Missing Required Quest Completion.
+        BattlergoundRestrictedAccount = 48,       // Free Trial Accounts Cannot Perform That Action
+        BattlegroundJoinMercenary = 49,       // Cannot Join As A Group Unless All The Members Of Your Party Are Flagged As A Mercenary.
+        BattlegroundJoinTooManyHealers = 51,       // You Can Not Enter This Bracket Of Arena With More Than One Healer. / You Can Not Enter A Rated Battleground With More Than Three Healers.
+        BattlegroundJoinTooManyTanks = 52,       // You Can Not Enter This Bracket Of Arena With More Than One Tank.
+        BattlegroundJoinTooManyDamage = 53,       // You Can Not Enter This Bracket Of Arena With More Than Two Damage Dealers.
+        GroupJoinBattlegroundDead = 57,       // You Cannot Join The Battleground Because You Or One Of Your Party Members Is Dead.
+        BattlegroundJoinRequiresLevel = 58,       // Tournament Rules Requires All Participants To Be Max Level.
+        BattlegroundJoinDisqualified = 59,       // %S Has Been Disqualified From Ranked Play In This Bracket.
+        ArenaExpiredCais = 60,       // You May Not Queue While One Or More Of Your Team Members Is Under The Effect Of Restricted Play.
+        SoloShuffleWargameGroupSize = 64,       // Exactly 6 Non-Spectator Players Must Be Present To Begin A Solo Shuffle Wargame.
+        SoloShuffleWargameGroupComp = 65,       // Exactly 4 Dps, And Either 2 Tanks Or 2 Healers, Must Be Present To Begin A Solo Shuffle Wargame.
     }
 
     public enum ScoreType
