b4d6607c18eb318a63ff4d153f81e77ddbe8cfbb
hondacrx <prelude_20032002@yahoo.com>
2023-01-06 17:32:11 -0500

    Core/Spells: Fixed crashes with spells cast by gameobject that summon creatures
    Port From (https://github.com/TrinityCore/TrinityCore/commit/fa361a40c81c5b1623fe2ff8f9a206868a6604f0)

--- Source/Game/Spells/SpellEffects.cs
+++ Source/Game/Spells/SpellEffects.cs
@@ -1592,7 +1592,7 @@ namespace Game.Spells
                                     // randomize position for multiple summons
                                     pos = caster.GetRandomPoint(destTarget, radius);
 
-                                summon = unitCaster.GetMap().SummonCreature(entry, pos, properties, (uint)duration, unitCaster, m_spellInfo.Id, 0, privateObjectOwner);
+                                summon = caster.GetMap().SummonCreature(entry, pos, properties, (uint)duration, unitCaster, m_spellInfo.Id, 0, privateObjectOwner);
                                 if (summon == null)
                                     continue;
 
