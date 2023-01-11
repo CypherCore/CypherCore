dd8d17a4da3b5209f40cd61e79dbba4a74554807
hondacrx <prelude_20032002@yahoo.com>
2023-01-11 01:26:59 -0500

    Core/DBUpdater: Changed autoupdater created databases to use utf8mb4 instead of utf8mb3
    Port From (https://github.com/TrinityCore/TrinityCore/commit/65e787d18be898532363fea3f8847fa4d7f21192)

--- Source/Framework/Database/DatabaseLoader.cs
+++ Source/Framework/Database/DatabaseLoader.cs
@@ -116,7 +116,7 @@ namespace Framework.Database
             try
             {
                 using BinaryWriter binaryWriter = new(File.Open(temp, FileMode.Create, FileAccess.Write));
-                binaryWriter.Write($"CREATE DATABASE `{connectionObject.Database}` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+                binaryWriter.Write($"CREATE DATABASE `{connectionObject.Database}` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
             }
             catch (Exception)
             {
