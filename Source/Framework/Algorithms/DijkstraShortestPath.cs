f73e64396f52920005fceca245cb72c9568093b3
hondacrx <prelude_20032002@yahoo.com>
2023-01-18 17:25:32 -0500

    Update license headers for all files.

--- Source/Framework/Algorithms/DijkstraShortestPath.cs
+++ Source/Framework/Algorithms/DijkstraShortestPath.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) CypherCore <http://github.com/CypherCore> All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using Framework.Collections;
 
--- Source/Framework/Database/Databases/WorldDatabase.cs
+++ Source/Framework/Database/Databases/WorldDatabase.cs
@@ -1,19 +1,5 @@
-﻿/*
- * Copyright (C) 2012-2020 CypherCore <http://github.com/CypherCore>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+﻿// Copyright (c) CypherCore <http://github.com/CypherCore> All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE file in the project root for full license information.
  
 namespace Framework.Database
 {
