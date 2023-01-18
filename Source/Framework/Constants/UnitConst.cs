df78d547ab997b508777c1aac835983558312146
hondacrx <prelude_20032002@yahoo.com>
2023-01-18 17:28:36 -0500

    Cleanups

--- Source/Framework/Constants/UnitConst.cs
+++ Source/Framework/Constants/UnitConst.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) CypherCore <http://github.com/CypherCore> All rights reserved.
 // Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE file in the project root for full license information.
+
 using System;
 
 namespace Framework.Constants
