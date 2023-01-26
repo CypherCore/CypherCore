10853611a5ce5172fcedc6c67a7c509ac9c8877c
Jim Burlison <JBurlison@gmail.com>
2023-01-26 08:14:29 -0500

    Removed unused methods

--- Source/Game/Scripting/ScriptObject.cs
+++ Source/Game/Scripting/ScriptObject.cs
@@ -31,26 +31,6 @@ namespace Game.Scripting
             return null;
         }
 
-        public static void ClearGossipMenuFor(Player player) { player.PlayerTalkClass.ClearMenus(); }
-        // Using provided text, not from DB
-        public static void AddGossipItemFor(Player player, GossipOptionNpc optionNpc, string text, uint sender, uint action)
-        {
-            player.PlayerTalkClass.GetGossipMenu().AddMenuItem(0, -1, optionNpc, text, 0, GossipOptionFlags.None, null, 0, 0, false, 0, "", null, null, sender, action);
-        }
-        // Using provided texts, not from DB
-        public static void AddGossipItemFor(Player player, GossipOptionNpc optionNpc, string text, uint sender, uint action, string popupText, uint popupMoney, bool coded)
-        {
-            player.PlayerTalkClass.GetGossipMenu().AddMenuItem(0, -1, optionNpc, text, 0, GossipOptionFlags.None, null, 0, 0, coded, popupMoney, popupText, null, null, sender, action);
-        }
-        // Uses gossip item info from DB
-        public static void AddGossipItemFor(Player player, uint gossipMenuID, uint gossipMenuItemID, uint sender, uint action)
-        {
-            player.PlayerTalkClass.GetGossipMenu().AddMenuItem(gossipMenuID, gossipMenuItemID, sender, action);
-        }
-        public static void SendGossipMenuFor(Player player, uint npcTextID, ObjectGuid guid) { player.PlayerTalkClass.SendGossipMenu(npcTextID, guid); }
-        public static void SendGossipMenuFor(Player player, uint npcTextID, Creature creature) { if (creature) SendGossipMenuFor(player, npcTextID, creature.GetGUID()); }
-        public static void CloseGossipMenuFor(Player player) { player.PlayerTalkClass.SendCloseGossip(); }
-
         string _name;
     }
 
