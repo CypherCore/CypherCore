0da22fc0084ec1e56715cb260d4947793ea7c354
Jim Burlison <JBurlison@Gmail.com>
2023-01-29 22:27:34 -0500

    Started warrior spells

--- Source/Game/Scripting/Interfaces/IAura/IAuraApplyHandler.cs
+++ Source/Game/Scripting/Interfaces/IAura/IAuraApplyHandler.cs
@@ -16,7 +16,7 @@ namespace Game.Scripting.Interfaces.IAura
 
         private readonly AuraEffectApplicationModeDelegate _fn;
 
-        public EffectApplyHandler(AuraEffectApplicationModeDelegate fn, uint effectIndex, AuraType auraType, AuraEffectHandleModes mode, AuraScriptHookType hookType) : base(effectIndex, auraType, hookType)
+        public EffectApplyHandler(AuraEffectApplicationModeDelegate fn, uint effectIndex, AuraType auraType, AuraEffectHandleModes mode, AuraScriptHookType hookType = AuraScriptHookType.EffectApply) : base(effectIndex, auraType, hookType)
         {
             _fn = fn;
             Modes = mode;
