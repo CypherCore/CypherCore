cd6d1ca2093643d41f192b7f904c6004c5bc97ad
Jim Burlison <JBurlison@Gmail.com>
2023-02-12 13:25:16 -0500

    added script to fix inkeepers so you can set your hearth

---
+++ sql/updates/world/Forged/innkeeperfix.sql
@@ -0,0 +1,23 @@
+update creature_template SET npcflag = 65536 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 0;
+
+update creature_template SET npcflag = 65537 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 1;
+
+update creature_template SET npcflag = 65538 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 2;
+
+update creature_template SET npcflag = 65539 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 3;
+
+update creature_template SET npcflag = 65664 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 128;
+
+update creature_template SET npcflag = 65665 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 129;
+
+update creature_template SET npcflag = 65666 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 130;
+
+update creature_template SET npcflag = 66176 WHERE subname LIKE "%Innkeeper"
+AND npcflag = 640;
