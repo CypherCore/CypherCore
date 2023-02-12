569b31d7c123486ca5c6b56c1a0d8380aa4d1443
Jim Burlison <JBurlison@Gmail.com>
2023-02-12 09:17:08 -0500

    Fix startup error

--- Source/Scripts/Events/LoveIsInTheAir.cs
+++ Source/Scripts/Events/LoveIsInTheAir.cs
@@ -200,7 +200,7 @@ namespace Scripts.m_Events.LoveIsInTheAir
 
         public override void Register()
         {
-            if (GetTargetApplication().GetRemoveMode() == AuraRemoveMode.Expire)
+            if (GetTargetApplication() != null && GetTargetApplication().GetRemoveMode() == AuraRemoveMode.Expire)
                 GetTarget().CastSpell(GetTarget(), SpellIds.HeavilyPerfumed);
         }
 
