e838f2dfcacb7dc91a8d317127f88e406c4d4c44
Cory Burlison <33432301+CBurlison@users.noreply.github.com>
2023-02-13 18:36:36 -0500

    SQL scripts for missing tables and shaman enhancement spell duration

---
+++ sql/updates/hotfixes/forged/spell_DF_Tables.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS `trait_system`;
+CREATE TABLE `trait_system` (
+  `ID` INT NOT NULL,
+  `Field_10_0_0_44795_001` INT NOT NULL,
+  `WidgetSetID` INT NOT NULL,
+  `VerifiedBuild` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`VerifiedBuild`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
+
+DROP TABLE IF EXISTS `spell`;
+CREATE TABLE `spell` (
+  `ID` INT NOT NULL,
+  `NameSubtext` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `Description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `AuraDescription` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `VerifiedBuild` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`VerifiedBuild`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
+
+DROP TABLE IF EXISTS `spell_replacement`;
+CREATE TABLE `spell_replacement` (
+  `ID` INT NOT NULL,
+  `SpellID` INT NOT NULL,
+  `VerifiedBuild` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`VerifiedBuild`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
+
+DROP TABLE IF EXISTS `spell_empower`;
+CREATE TABLE `spell_empower` (
+  `ID` INT NOT NULL,
+  `SpellID` INT NOT NULL,
+  `OtherValue` INT NOT NULL,
+  `VerifiedBuild` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`VerifiedBuild`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
+
+DROP TABLE IF EXISTS `spell_empower_stage`;
+CREATE TABLE `spell_empower_stage` (
+  `ID` INT NOT NULL,
+  `SpellEmpowerStage` INT NOT NULL,
+  `OtherValue` INT NOT NULL,
+  `VerifiedBuild` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`,`VerifiedBuild`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
---
+++ sql/updates/hotfixes/forged/spell_item_enchantment.sql
@@ -0,0 +1,1 @@
+UPDATE spell_item_enchantment SET Duration = 3600 WHERE ID IN (5398, 5399, 5400, 5401, 5402);
