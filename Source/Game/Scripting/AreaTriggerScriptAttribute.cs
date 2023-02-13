2161e61718cae10df0813753b4d18ae26c8fc0f4
Jim Burlison <JBurlison@Gmail.com>
2023-02-13 16:12:15 -0500

    Added area trigger script attribute

---
+++ Source/Game/Scripting/AreaTriggerScriptAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Scripting
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class AreaTriggerScriptAttribute : ScriptAttribute
+    {
+        public AreaTriggerScriptAttribute(string name = "", params object[] args) : base(name, args)
+        {
+        }
+
+        public AreaTriggerScriptAttribute(uint areaTriggerId, string name = "", params object[] args) : base(name, args)
+        {
+            AreaTriggerIds = new[]
+                       {
+                           areaTriggerId
+                       };
+        }
+
+        public AreaTriggerScriptAttribute(uint[] areaTriggerIds, string name = "", params object[] args) : base(name, args)
+        {
+            AreaTriggerIds = areaTriggerIds;
+        }
+
+        public uint[] AreaTriggerIds { get; private set; }
+    }
+}
---
+++ Source/Game/Scripting/Registers/AreaTriggerRegister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Entities;
+using Game.Scripting.Interfaces;
+
+namespace Game.Scripting.Registers
+{
+    public class AreaTriggerRegister : IScriptRegister
+    {
+        public Type AttributeType => typeof(AreaTriggerScriptAttribute);
+
+        public void Register(ScriptAttribute attribute, IScriptObject script, string scriptName)
+        {
+            if (attribute is AreaTriggerScriptAttribute spellScript && spellScript.AreaTriggerIds != null)
+                foreach (var id in spellScript.AreaTriggerIds)
+                {
+                    var atcp = Global.AreaTriggerDataStorage.GetAreaTriggerCreateProperties(id);
+
+                    if (atcp != null && atcp.ScriptId != 0)
+                        atcp.ScriptId = Global.ObjectMgr.GetScriptId(scriptName);
+                }
+        }
+
+    }
+}
