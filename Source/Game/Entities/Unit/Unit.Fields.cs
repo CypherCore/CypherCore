e5ad9b6150ad9adead78f8711fcc009be7378f15
Jim Burlison <JBurlison@Gmail.com>
2023-02-20 15:51:22 -0500

    Fix damage logging. Fix aborbs on melee being calculated before damage is scaled. Fix PWS

--- Source/Game/Entities/Unit/Unit.Fields.cs
+++ Source/Game/Entities/Unit/Unit.Fields.cs
@@ -388,6 +388,8 @@ namespace Game.Entities
         Unit m_attacker;
         Unit m_victim;
         uint m_damage;
+        uint m_damageDone;
+        uint m_damageTaken;
         uint m_originalDamage;
         SpellInfo m_spellInfo;
         SpellSchoolMask m_schoolMask;
