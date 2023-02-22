8b36426c72a24718c3414619cc70a93ecdbfc521
hater <hater.rip@proton.me>
2023-02-22 17:57:29 -0500

    remove doubled conditional

--- Source/Game/Spells/Spell.cs
+++ Source/Game/Spells/Spell.cs
@@ -7407,7 +7407,7 @@ namespace Game.Spells
                 {
                     foreach (var iFace in script.GetType().GetInterfaces())
                     {
-                        if (iFace.Name == nameof(ISpellScript) || iFace.Name == nameof(ISpellScript) || iFace.Name == nameof(IBaseSpellScript))
+                        if (iFace.Name == nameof(ISpellScript) || iFace.Name == nameof(IBaseSpellScript))
                             continue;
 
                         if (!m_spellScriptsByType.TryGetValue(iFace, out var spellScripts))
