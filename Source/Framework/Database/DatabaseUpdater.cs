3a2e64dba498bbae8e18657a0af8614eecd66899
hondacrx <prelude_20032002@yahoo.com>
2023-04-21 19:19:31 -0400

    TDB 1007.23041 - 2023/04/02
    Port From (https://github.com/TrinityCore/TrinityCore/commit/1b588502e3afddb2312514d4ac9d87a9640e7dfd)

--- Source/Framework/Database/DatabaseUpdater.cs
+++ Source/Framework/Database/DatabaseUpdater.cs
@@ -37,10 +37,10 @@ namespace Framework.Database
                     fileName = @"/sql/base/characters_database.sql";
                     break;
                 case "WorldDatabase":
-                    fileName = @"/sql/TDB_full_world_1005.23021_2023_02_03.sql";
+                    fileName = @"/sql/TDB_full_world_1005.23041_2023_04_02.sql";
                     break;
                 case "HotfixDatabase":
-                    fileName = @"/sql/TDB_full_hotfixes_1005.23021_2023_02_03.sql";
+                    fileName = @"/sql/TDB_full_hotfixes_1005.23041_2023_04_02.sql";
                     break;
             }
 
