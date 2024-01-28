08817b7a906d523080042fa586853429c2a81cd3
RioMcBoo <artem.jkl@gmail.com>
2024-01-28 08:06:09 +0200

    Fix exception when calculating for december

--- Source/Game/World/WorldManager.cs
+++ Source/Game/World/WorldManager.cs
@@ -2100,8 +2100,7 @@ namespace Game
             if (time.Day == 1)
                 return t;
 
-            var newDate = new DateTime(time.Year, time.Month + 1, 1, 0, 0, 0, time.Kind);
-            return Time.DateTimeToUnixTime(newDate);
+            return Time.DateTimeToUnixTime(time.AddMonths(1));
         }
 
         public void ResetMonthlyQuests()
