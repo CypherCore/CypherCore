ab6d82af0bbf8d6c456ceba5f70009ebd361aa60
Hondacrx <prelude_20032002@yahoo.com>
2026-07-26 12:54:31 -0400

    Core/Misc: Update CURRENT_EXPANSION to Midnight
    Port From (https://github.com/TrinityCore/TrinityCore/commit/592e6381a91a84fd5956b64de1c4eddbd5af8dcf)

--- Source/Framework/Constants/PlayerConst.cs
+++ Source/Framework/Constants/PlayerConst.cs
@@ -7,7 +7,7 @@ namespace Framework.Constants
 {
     public struct PlayerConst
     {
-        public const Expansion CurrentExpansion = Expansion.TheWarWithin;
+        public const Expansion CurrentExpansion = Expansion.Midnight;
 
         public const int MaxTalentTiers = 7;
         public const int MaxTalentColumns = 4;
